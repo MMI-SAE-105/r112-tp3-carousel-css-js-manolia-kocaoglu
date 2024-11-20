6f02a52963088afa8f7a523b30d7aa3d4fe888a9
manolia-kocaoglu <manolia.kocaoglu@edu.univ-fcomte.fr>
2024-11-20 14:36:05 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,21 @@
 // *** CAROUSEL ***
 // TODO
-const carousel = document.querySelector(".carousel__container");
-const prevButton = document.querySelector(".carousel__button--prev");
-const nextButton = document.querySelector(".carousel__button--next");
+const carousel = document.querySelector(".carouselcontainer");
+const prevButton = document.querySelector(".carouselbutton--prev");
+const nextButton = document.querySelector(".carouselbutton--next");
+
+const premierItem = document.querySelector(".carouselitem");
+const scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d’un item
 if (carousel) {
   // Scroll au clic sur le bouton précédent
   prevButton.addEventListener("click", () => {
-    carousel.scrollBy({ left: -260, behavior: "smooth" });;
+    carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });;
   });
 
   // Scroll au clic sur le bouton suivant
   nextButton.addEventListener("click", () => {
-    carousel.scrollBy({ left: +260, behavior: "smooth" });;
+    carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });;
   });
 }
